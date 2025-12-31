4056ed9a98f5e87520be52f00bbbbad757debac9
syedkaif07 <syedkaifshahsahab@gmail.com>
2026-01-01 03:45:16 +0500

    Remaining prjoect

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let obj = {
     date: '',
     note: '',
     id: '',
-    srch: '',
 }
 // target object keys and value..
 
@@ -16,21 +15,20 @@ function handleChange(event) {
     obj[key] = value;
     obj.id = Math.random() * 100 + 1
     // obj.id = crypto.randomUUID();  // generate unique ID
- 
+
 }
 
 
-
-   let btn = document.getElementById('type1');
-   btn.addEventListener('click',()=>{
-     btn.classList.add('active');
+let btn = document.getElementById('type1');
+btn.addEventListener('click', () => {
+    btn.classList.add('active');
     console.log(btn);
-   });
+});
 
-     let btn2 = document.getElementById("type2");
-    btn2.addEventListener('click',()=>{
-        btn2.classList.add('active')
-    });
+let btn2 = document.getElementById("type2");
+btn2.addEventListener('click', () => {
+    btn2.classList.add('active')
+});
 
 // Show data in Html....
 function showdata() {
@@ -41,14 +39,14 @@ function showdata() {
         tablebody.innerHTML += `
         <tr class="text-secondary">
         <td onclick="Model(${items.id})" scope="col" class="text-start"  data-bs-toggle="modal"
-      data-bs-target="#mewModal">${items.title}</td>
+      data-bs-target="#newModal">${items.title}</td>
         <td scope="col" class="text-start">${items.amount}</td>
         <td scope="col" class="text-start">${items.type}</td>
         <td scope="col" class="text-start">${items.Category}</td>
         <td scope="col" class="text-start">${items.date}</td>
         <td scope="col" class="text-start">${items.note}</td>
         <td scope="col" class="text-start">
-        <svg onclick="edit(${items.id})" class="text-primary" data-bs-toggle="modal" data-bs-target="#EditBox"
+         <svg onclick="edit(${items.id})" class="text-primary" data-bs-toggle="modal" data-bs-target="#EditBox"
               data-bs-whatever="@getbootstrap"   xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.854a.5.5 0 0 1 .708 0l2.292 2.292a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2L2 11.207V13h1.793L14 3.793 11.207 2zM15 1.5L13.5 0 12 1.5 13.5 3 15 1.5z"/>
 </svg>
@@ -62,18 +60,6 @@ function showdata() {
             ;
     });
 }
-
-// Edit Form..
-// if (!item || !ele) return;
-// if (item.type === 'Income') {
-//     ele.value = item.type;
-//     ele.classList.add('IncomeActive');
-// } else {
-//     ele.classList.remove('IncomeActive');
-//     if (ele2) {
-//         ele2.value = item.type;
-//     }
-// }
 
 
 function edit(id) {
@@ -93,9 +79,9 @@ function edit(id) {
     if (item.type === 'Income') {
         type1.value = item.type;
         type1.classList.add('active');      // active class add
-        type2.classList.remove('active'); 
-        console.log(type1,type2);
-          // dusre se remove
+        type2.classList.remove('active');
+        console.log(type1, type2);
+        // dusre se remove
     } else {
         type2.value = item.type;
         type2.classList.add('active');
@@ -104,16 +90,16 @@ function edit(id) {
 
 }
 
-    const type = document.getElementById('E-type1');
-    type.addEventListener('click',()=>{
-        type.classList.add('active')
-     
-    })
-     const type2 = document.getElementById('E-type2');
-    type2.addEventListener('click',()=>{
-        type2.classList.add('active')
-       
-    })
+const type = document.getElementById('E-type1');
+type.addEventListener('click', () => {
+    type.classList.add('active')
+
+})
+const type2 = document.getElementById('E-type2');
+type2.addEventListener('click', () => {
+    type2.classList.add('active')
+
+})
 
 
 // Update Data...
@@ -279,7 +265,7 @@ function ChangeCategory(selectedType) {
         <td scope="col" class="text-start">${items.note}</td>
         <td scope="col" class="text-start">
         <svg onclick="edit(${items.id})" class="text-primary" data-bs-toggle="modal" data-bs-target="#EditBox"
-              data-bs-whatever="@getbootstrap"   xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+              data-bs-whatever="@getbootstrap" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.854a.5.5 0 0 1 .708 0l2.292 2.292a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2L2 11.207V13h1.793L14 3.793 11.207 2zM15 1.5L13.5 0 12 1.5 13.5 3 15 1.5z"/>
 </svg>
 <svg onclick="deleteicon(${items.id})"  id"deleteicon" class="ms-3 text-danger" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
@@ -299,11 +285,16 @@ function search(value) {
     let data = JSON.parse(localStorage.getItem('transaction')) || []
     let tablebody = document.getElementById('tbody');
     tablebody.innerHTML = '';
-    let result = data.filter((item) => item.Category.toLowerCase().includes(value.toLowerCase()))
-      result.forEach(items => {
+    let result = data.filter(item => item.Category.toLowerCase().includes(value.toLowerCase()))
+    let NotFound = document.getElementById('NotFound')
+    NotFound.innerHTML = ''
+    if (result.length === 0) {
+        NotFound.innerHTML = 'Not Found'
+    }
+    result.forEach(items => {
         tablebody.innerHTML += `
         <tr class="text-secondary">
-        <td onclick="Model(${items.id})" scope="col" class="text-start"  data-bs-toggle="modal"
+        <td onclick="Model(${items.id})" scope="col" class="text-start "  data-bs-toggle="modal"
       data-bs-target="#mewModal">${items.title}</td>
         <td scope="col" class="text-start">${items.amount}</td>
         <td scope="col" class="text-start">${items.type}</td>
@@ -315,16 +306,17 @@ function search(value) {
               data-bs-whatever="@getbootstrap"   xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
   <path d="M12.146.854a.5.5 0 0 1 .708 0l2.292 2.292a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2L2 11.207V13h1.793L14 3.793 11.207 2zM15 1.5L13.5 0 12 1.5 13.5 3 15 1.5z"/>
 </svg>
-<svg onclick="deleteicon(${items.id})"  id"deleteicon" class="ms-3 text-danger" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+<svg onclick="deleteicon(${items.id})"  id"deleteicon"  class="text-danger" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5zm-1-1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v1H4.5v-1z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1V2a.5.5 0 0 1 .5-.5H5.5l.5-.5h4l.5.5h3.5a.5.5 0 0 1 .5.5v1zM4.118 4 4 14.5A1.5 1.5 0 0 0 5.5 16h5a1.5 1.5 0 0 0 1.5-1.5L11.882 4H4.118z"/>
 </svg>
 </td> </tr>
+
   
 `   ;
-console.log(value);
 
-
+        console.log(value);
+        console.log("result", result);
 
 
     });
@@ -339,15 +331,4 @@ console.log(value);
 
 // console.log(item.Category === selectedType); // false, kyunki "Food" != "food"
 // console.log(item.Category.toLowerCase() === selectedType.toLowerCase()); // true
-
-
-
-
-
-
-
-
-
-
-
 
